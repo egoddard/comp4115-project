4edf55de6e7347823fe40f208d81e81935ef5b86
Eric Goddard <egoddard@memphis.edu>
2015-04-26 10:26:05 -0500

    Add indexes and constraints.

--- scripts/schema_transformation.sql
+++ scripts/schema_transformation.sql
@@ -1,4 +1,4 @@
-drop table if exists facilities;
+drop table if exists facilities cascade;
 drop table if exists env_issues;
 drop table if exists hyperlinks;
 drop table if exists property_types;
@@ -6,7 +6,7 @@ drop table if exists facility_types;
 drop table if exists wellhead_protection_zones;
 
 create table facilities(
-    ogc_fid serial not null primary key, 
+    ogc_fid serial not null primary key,
     wkb_geometry geometry(Point,2274),  
     facility_id text,
     facility_name text,
@@ -42,4 +42,14 @@ create table wellhead_protection_zones(
     well_field text,
     aquifer text, 
     wkb_geometry geometry(MultiPolygon,2274));
+
+create index facilities_whpa_idx on facilities(whpa_id);
+create unique index facilities_fac_idx on facilities(facility_id);
+create index env_issues_idx on env_issues(env_id);
+create index env_issues_fac_idx on env_issues(facility_id);
+create index hyperlinks_fac_idx on hyperlinks(facility_id);
+
+alter table env_issues add constraint env_facid_fk foreign key (facility_id) references facilities (facility_id);
+alter table hyperlinks add constraint hyperlink_facid_fk foreign key (facility_id) references facilities (facility_id);
+
 
